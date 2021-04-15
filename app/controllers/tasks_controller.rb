a745dc406686db96ef1354d346618957f195fd98
furukawa <guppy872@gmail.com>
2021-04-15 03:32:41 +0000

    lesson15_kadai

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,14 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = Task.all
+    if logged_in?
+      @task = current_user.tasks.build
+      @tasks = current_user.tasks.order(id: :desc).page(params[:psge])
+    else
+      redirect_to login_path
+    end
   end 
   
   def show 
@@ -13,7 +19,7 @@ class TasksController < ApplicationController
   end 
   
   def create 
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = 'Task は正常に投稿されました'
@@ -47,12 +53,15 @@ class TasksController < ApplicationController
   end 
   
   private
-  
-  def set_task
-    @task = Task.find(params[:id])
-  end
 
   def task_params
     params.require(:task).permit(:content, :status)
   end 
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 end
